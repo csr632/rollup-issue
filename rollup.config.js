1d0843cdb8bb2835eb7b1e605bf23fffa7fe40b4
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-15 16:29:28 +0800

    simplify

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,14 @@
 import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: {
-    // doesn't work when have multiple inputs
-    react: "react",
-    "react-dom": "react-dom",
+    t1: "test1.js",
+    t2: "test2.js",
   },
-  plugins: [
-    commonjs({
-      namedExports: {
-        react: detectExports("react"),
-        "react-dom": detectExports("react-dom"),
-      },
-    }),
-    nodeResolve({
-      mainFields: ["browser:module", "module", "browser", "main"],
-      extensions: [".mjs", ".cjs", ".js", ".json"],
-    }),
-  ],
+  plugins: [commonjs()],
   output: {
     format: "esm",
     dir: "dist",
     exports: "named",
   },
 };
-
-function detectExports(filePath) {
-  try {
-    const fileLoc = require.resolve(filePath, { paths: [cwd] });
-    if (fs.existsSync(fileLoc)) {
-      return Object.keys(require(fileLoc)).filter((e) => e[0] !== "_");
-    }
-  } catch (err) {
-    // ignore
-  }
-}
---
+++ test1.js
@@ -0,0 +1,6 @@
+const test2 = require("./test2");
+
+module.exports = {
+  b: 2,
+  test2,
+};
