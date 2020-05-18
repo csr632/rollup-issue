d0777790e640cee8e06ca06957ba91d783c92131
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-18 14:08:16 +0800

    save work

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ console.log(Nav.Item);
 
 import { Collapse } from "@alifd/next";
 console.log(Collapse.Panel);
+
+
+// import { Menu } from "test-package";
+
+// console.log(Menu);
+// console.log(Menu.Item);
+
+
+import { Menu } from "antd";
+
+console.log(Menu);
+console.log(Menu.Item);
