5a3320fe32eb7c6453c1aaf895f86f3b14e1a9d8
sirui.csr <sirui.csr@alibaba-inc.com>
2020-05-18 14:51:13 +0800

    fix build

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-import { Menu } from "@alifd/next";
-console.log(Menu.SubMenu);
+// import { Menu } from "@alifd/next";
+// console.log(Menu.SubMenu);
 
-import { Nav } from "@alifd/next";
-console.log(Nav.Item);
+// import { Nav } from "@alifd/next";
+// console.log(Nav.Item);
 
-import { Collapse } from "@alifd/next";
-console.log(Collapse.Panel);
+// import { Collapse } from "@alifd/next";
+// console.log(Collapse.Panel);
 
 
-// import { Menu } from "test-package";
+import { Menu } from "test-package";
+console.log(Menu);
+console.log(Menu.Item);
+
+
+// import { Menu } from "antd";
 
 // console.log(Menu);
 // console.log(Menu.Item);
-
-
-import { Menu } from "antd";
-
-console.log(Menu);
-console.log(Menu.Item);
